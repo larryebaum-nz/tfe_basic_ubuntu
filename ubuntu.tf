ee40649bfd961f9ecb07e6ab911ba8b92a179f11
Larry Eichenbaum <6776768+larryebaum@users.noreply.github.com>
2020-08-05 13:19:54 -0400

    Update ubuntu.tf

--- ubuntu.tf
+++ ubuntu.tf
@@ -1,22 +1,24 @@
 # Create a new instance of the latest Ubuntu 14.04 on an
 # t2.micro node with an AWS Tag naming it "HelloWorld"
 provider "aws" {
-  region = "us-west-2"
-}
-data "aws_ami" "ubuntu" {
-  most_recent = true
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+#  region = "us-west-2"
+  region = "us-gov-east-1"
   }
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-  owners = ["099720109477"] # Canonical
-}
+# data "aws_ami" "ubuntu" {
+#   most_recent = true
+#   filter {
+#     name   = "name"
+#     values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+#   }
+#   filter {
+#     name   = "virtualization-type"
+#     values = ["hvm"]
+#   }
+#   owners = ["099720109477"] # Canonical
+# }
 resource "aws_instance" "web" {
-  ami           = "${data.aws_ami.ubuntu.id}"
+#  ami           = "${data.aws_ami.ubuntu.id}"
+  ami = "ami-03c5d715986348a95"
   instance_type = "t2.micro"
   tags = {
     Name = "HelloWorld"
